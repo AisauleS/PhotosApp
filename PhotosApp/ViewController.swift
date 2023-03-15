43eca6b6e99ea38f4e2a69fa0808d29fe8164b4e
AisauleS <aisaulesibatova@gmail.com>
2023-03-16 02:13:55 +0500

    Initial Commit

---
+++ PhotosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Aisaule Sibatova on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
